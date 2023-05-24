b1423e4a7b8f6bf5e716834770352dd53eaadb5b
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-24 05:03:55 -0300

    prisma test suite

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,34 @@
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import { Environment } from 'vitest'
+import { prisma } from '../../src/lib/prisma'
+
+// postgresql://docker:docker@localhost:5432/apigym?schema=public
+function generateDbURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
+
+export default <Environment>{
+  name: 'prisma',
+  async setup() {
+    const schema = randomUUID()
+    const dbURL = generateDbURL(schema)
+
+    process.env.DATABASE_URL = dbURL
+    execSync('npx prisma migrate deploy') // deploy skips schema comparison, is faster than dev
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema} CASCADE"`,
+        )
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
